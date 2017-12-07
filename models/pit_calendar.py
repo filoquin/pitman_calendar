9d472d78827566ad22cc2a29342016a2b01cbe80
filoquin <filoquin>
2017-12-07 08:37:00 -0300

    - Borre si queres los archios idea. Perdon - Modifico las vista para tener vistas de calendario, tree y form :to-do ver si sacamos el quick_add - agrego el campo name a pitman_calendar - agrego el campo group_calendar para mantener una relacion con el registro que lo creara (asi facilito la trasabilidad) - Agrego el aula para poder agrupar y ver calendarios por aula to-do - falta creacion de calendarios de clase de manera automatica - falta vistas de busqueda y agrupado

--- models/pit_calendar.py
+++ models/pit_calendar.py
@@ -5,8 +5,18 @@ class pit_calendar(models.Model):
     _name = "pit.calendar"
     _description = "Calendar"
 
-    start_date = fields.Date()
-    end_date = fields.Date()
+
+    name = fields.Char('Name',compute='compute_name',store=True)
+    start_date = fields.Datetime(required=True)
+    end_date = fields.Datetime()
     teacher_id = fields.Many2one('pit.teacher', 'Teacher', required=True)
     group_id = fields.Many2one('pit.school.course.group', 'Group')
     calendar_items_ids = fields.One2many('pit.calendar.item', 'calendar_id', string='Items')
+    group_calendar_id = fields.Many2one('pit.school.course.calendar',string='Group Calendar')
+    classroom_id = fields.Many2one('pit.location.classroom','Classroom',related="group_calendar_id.classroom_id",store=True)
+
+
+    @api.depends('start_date','group_id','teacher_id')
+    @api.one
+    def _compute_name(self):
+        self.name = "%s %s %s" % (self.start_date ,self.teacher_id.name, self.group_id.name)
